36cd9c497967cecb7b1a1fde8d693464117c8e5e
Christopher David <chris@arcadelabs.co>
2024-06-25 19:10:48 -0500

    Webserver runs

--- go.mod
+++ go.mod
@@ -3,10 +3,14 @@ module github.com/OpenAgentsInc/autodev
 go 1.22.1
 
 require (
-	github.com/a-h/templ v0.2.707 // indirect
-	github.com/extism/go-sdk v1.2.0 // indirect
+	github.com/a-h/templ v0.2.707
+	github.com/extism/go-sdk v1.2.0
+	github.com/labstack/echo/v4 v4.12.0
+)
+
+require (
 	github.com/gobwas/glob v0.2.3 // indirect
-	github.com/labstack/echo/v4 v4.12.0 // indirect
+	github.com/golang-jwt/jwt v3.2.2+incompatible // indirect
 	github.com/labstack/gommon v0.4.2 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
@@ -17,4 +21,5 @@ require (
 	golang.org/x/net v0.24.0 // indirect
 	golang.org/x/sys v0.19.0 // indirect
 	golang.org/x/text v0.14.0 // indirect
+	golang.org/x/time v0.5.0 // indirect
 )
--- main.go
+++ main.go
@@ -3,14 +3,31 @@ package main
 import (
 	"context"
 	"fmt"
+	"io"
 	"net/http"
 
+	"github.com/OpenAgentsInc/autodev/views"
 	"github.com/extism/go-sdk"
 	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 )
+
+// Templ renderer
+type TemplRenderer struct{}
+
+func (t *TemplRenderer) Render(w io.Writer, name string, data interface{}, c echo.Context) error {
+	return views.Index().Render(context.Background(), w)
+}
 
 func main() {
 	e := echo.New()
+
+	// Add logging middleware
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+
+	// Set up Templ renderer
+	e.Renderer = &TemplRenderer{}
 
 	// Serve static files
 	e.Static("/static", "static")
@@ -22,36 +39,47 @@ func main() {
 				Path: "plugins/wasm/greptile.wasm",
 			},
 		},
+		AllowedHosts: []string{"api.greptile.com"},
+	}
+
+	config := extism.PluginConfig{
+		EnableWasi: true,
 	}
 
 	ctx := context.Background()
-	config := extism.PluginConfig{}
 	plugin, err := extism.NewPlugin(ctx, manifest, config, []extism.HostFunction{})
 	if err != nil {
 		e.Logger.Fatal(err)
 	}
+	defer plugin.Close()
 
 	// Define routes
 	e.GET("/", func(c echo.Context) error {
-		return c.Render(http.StatusOK, "index.html", nil)
+		e.Logger.Info("Rendering index template")
+		return c.Render(http.StatusOK, "index", nil)
 	})
 
 	e.POST("/run-plugin", func(c echo.Context) error {
 		operation := c.FormValue("operation")
 		input := c.FormValue("input")
 
+		e.Logger.Infof("Running plugin with operation: %s, input: %s", operation, input)
+
 		// Prepare input for the plugin
 		pluginInput := fmt.Sprintf(`{"operation":"%s","repository":"%s"}`, operation, input)
 
 		// Call the plugin
-		exit, out, err := plugin.Call("run", []byte(pluginInput))
+		exitCode, out, err := plugin.Call("run", []byte(pluginInput))
 		if err != nil {
+			e.Logger.Errorf("Plugin call error: %v", err)
 			return c.JSON(http.StatusInternalServerError, map[string]string{"error": err.Error()})
 		}
-		if exit != 0 {
-			return c.JSON(http.StatusInternalServerError, map[string]string{"error": "Plugin exited with non-zero status"})
+		if exitCode != 0 {
+			e.Logger.Errorf("Plugin exited with non-zero code: %d", exitCode)
+			return c.JSON(http.StatusInternalServerError, map[string]string{"error": fmt.Sprintf("Plugin exited with code %d", exitCode)})
 		}
 
+		e.Logger.Infof("Plugin call successful, output: %s", string(out))
 		return c.JSON(http.StatusOK, map[string]string{"result": string(out)})
 	})
 
