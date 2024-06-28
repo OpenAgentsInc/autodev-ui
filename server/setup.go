ebd25d24e3885d623a4a3d82846643cb677ddab7
Christopher David <chris@arcadelabs.co>
2024-06-28 01:56:31 -0500

    revert setupserver

--- server/setup.go
+++ server/setup.go
@@ -12,32 +12,10 @@ import (
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
 	"github.com/openagentsinc/autodev/config"
-	"github.com/openagentsinc/autodev/pkg/action"
-	"github.com/openagentsinc/autodev/pkg/agent"
-	"github.com/openagentsinc/autodev/pkg/plugin"
-	"github.com/openagentsinc/autodev/pkg/state"
 	"github.com/openagentsinc/autodev/pkg/wanix/githubfs"
 	"github.com/openagentsinc/autodev/plugins"
 	"github.com/openagentsinc/autodev/views"
 )
-
-type ConversationEntry struct {
-	Role    string
-	Content string
-}
-
-type AgentController struct {
-	agent   func() agent.Agent
-	Actions action.ActionManager
-}
-
-func (ac *AgentController) ActionManager() action.ActionManager {
-	return ac.Actions
-}
-
-func (ac *AgentController) Agent() agent.Agent {
-	return ac.agent()
-}
 
 func SetupServer(cfg *config.Config, extismPlugin *extism.Plugin) *echo.Echo {
 	e := echo.New()
@@ -45,50 +23,6 @@ func SetupServer(cfg *config.Config, extismPlugin *extism.Plugin) *echo.Echo {
 	e.Use(middleware.Recover())
 	e.Renderer = &TemplRenderer{}
 	e.Static("/static", "static")
-
-	conversation := make([]ConversationEntry, 0)
-	pluginReqs := []plugin.PluginRequirement{
-		// Add your plugin requirements here, for example:
-		// {Name: "example-plugin", Version: "1.0.0"},
-	}
-	agentCtrl := &AgentController{
-		agent: func() agent.Agent {
-			return agent.NewBaseAgent(cfg.LLM, pluginReqs)
-		},
-		Actions: action.NewActionManager(), // Make sure this function exists in the action package
-	}
-
-	e.GET("/agent/test", func(c echo.Context) error {
-		return c.Render(http.StatusOK, "agent_test", conversation)
-	})
-
-	e.POST("/agent/interact", func(c echo.Context) error {
-		message := c.FormValue("message")
-		conversation = append(conversation, ConversationEntry{Role: "user", Content: message})
-
-		// Create a new state for each interaction
-		state := state.NewState(nil)
-
-		// Create a new state for each interaction
-		// state := state.NewState(message)
-
-		// Get the next action from the agent
-		nextAction := agentCtrl.Agent.Step(state)
-
-		// 0] server/setup.go:73:30: cannot use agentCtrl (variable of type *AgentController) as action.AgentController value in argument to nextAction.Run: *AgentController does not implement action.AgentController (*AgentController.Agent is a field, not a method)
-		// Execute the action
-		obs, err := nextAction.Run(agentCtrl)
-		// if err != nil {
-		// return c.JSON(http.StatusInternalServerError, map[string]string{"error": err.Error()})
-		// }
-
-		// Add the agent's response to the conversation
-		// agentResponse := obs.GetContent()
-		// conversation = append(conversation, ConversationEntry{Role: "agent", Content: agentResponse})
-		agentResponse := "Hello, I am an agent. How can I help you?"
-
-		return c.Render(http.StatusOK, "conversation_update", ConversationEntry{Role: "agent", Content: agentResponse})
-	})
 
 	cssVersion := fmt.Sprintf("v=%d", time.Now().Unix())
 
@@ -342,7 +276,6 @@ type TemplRenderer struct{}
 
 func (t *TemplRenderer) Render(w io.Writer, name string, data interface{}, c echo.Context) error {
 	viewContext, ok := data.(map[string]interface{})
-	ctx := context.Background()
 
 	if !ok {
 		return fmt.Errorf("invalid data type for rendering")
@@ -355,18 +288,6 @@ func (t *TemplRenderer) Render(w io.Writer, name string, data interface{}, c ech
 		return views.Index(cssVersion, viewContext).Render(context.Background(), w)
 	case "greptile":
 		return views.Greptile(cssVersion).Render(context.Background(), w)
-	case "agent_test":
-		conversation, ok := data.([]ConversationEntry)
-		if !ok {
-			return fmt.Errorf("unexpected data type for agent_test template")
-		}
-		return views.AgentTest(conversation).Render(ctx, w)
-	case "conversation_update":
-		entry, ok := data.(ConversationEntry)
-		if !ok {
-			return fmt.Errorf("unexpected data type for conversation_update template")
-		}
-		return views.ConversationUpdate(entry).Render(ctx, w)
 	case "file_explorer_widget":
 		repo, _ := viewContext["Repo"].(string)
 		branches, _ := viewContext["Branches"].([]string)
